40338514a140122f4cb8ef353f39ef8e7ace2d9b
CristhianCanar <cristhian.canar.m@gmail.com>
2024-04-30 18:51:14 -0500

    Primer commit

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('titulo', 'Sobre la moto')
+
+@section('contenido')
+    <p>Superar los límites de la ingeniería y el rendimiento es una de las pocas motocicletas que realmente
+        Merecen llamarse Instant Icons. Creado sin compromisos y beneficiándose de la
+        Fuerza colectiva de Kawasaki Heavy Industries, la Ninja H2R exige respeto
+        y atrae a los ciclistas más hábiles y comprometidos. Características del H2R: Ohlins
+        suspensión trasera, cambio rápido hacia arriba y hacia abajo, dispositivos aerodinámicos,
+        Gestión de curvas y visualización del ángulo de inclinación. Ninja H2R, jugar
+        tu parte en la historia. Este vehículo no está fabricado para
+        uso de la vía pública.</p>
+@endsection
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('titulo', 'Contactanos')
+
+@section('contenido')
+    <h1>Contacto</h1>
+    <p>Escribenos al WhatsApp +57 3215151755</p>
+
+@endsection
